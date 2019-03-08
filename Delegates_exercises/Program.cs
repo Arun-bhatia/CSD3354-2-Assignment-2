c1d2e281b72b37a82f79f0ea54d894fbccd72935
visualstudio <visualstudio@122-PC34>
2019-03-08 18:34:33 -0500

    Lab02

--- Delegates_exercises/Program.cs
+++ Delegates_exercises/Program.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//Arun bhatia(C0732127)
+//Harjeet Singh(C0730468)
 namespace Delegates_exercises
 {
-    class Program
+    class DelegateExercises
     {
-        static void Main(string[] args)
+        public delegate void MyDelegate(int value);
+
+        public static void Main(String[] args)
         {
+            MyDelegate myDelegate = Method2;
+            myDelegate(50);
         }
+
+         public static void Method2(int x)//static method
+        {
+            Console.WriteLine("Method2");
+            Console.WriteLine("You entered "+x);
+            Console.ReadLine();
+        }
+
+        
+
+
+        
     }
-}
+    }
+
