16fdc649778cee36ce9fbab99107435c66a8d0f4
visualstudio <visualstudio@122-PC34>
2019-03-08 19:29:51 -0500

    Lab05

--- Delegates_exercises/Program.cs
+++ Delegates_exercises/Program.cs
@@ -7,33 +7,51 @@ using System.Threading.Tasks;
 //Harjeet Singh(C0730468)
 namespace Delegates_exercises
 {
-    class DelegateExercises
+    class program
     {
-        public delegate void MyDelegate(int value);
+        //public delegate void MyDelegate();
 
         public static void Main(String[] args)
         {
-            MyDelegate myDelegate = Method1;
-            myDelegate+=Method2;
-            myDelegate(50);
-        }
-        public static void Method1(int num1)
-        {
-            Console.WriteLine(num1 * 2);
-        }
+            //    MyDelegate myDelegate = Method1;
+            //    myDelegate+=Method2;
+            //    myDelegate(50);
+            //}
+            //public static void Method1(int num1)
+            //{
+            //    Console.WriteLine(num1 * 2);
+            //}
 
+
+            // public static void Method2(int x)//static methodno parameters
+            //{
+            //    Console.WriteLine("Method 2 executed");
+            //    Console.WriteLine("You entered "+x);
+            //    Console.ReadLine();
+            //}
+
+            //public static void Method3(int num2)
+            //{
+            //    Console.WriteLine(num2 * 10);
+            //}
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
+        }
         
-         public static void Method2(int x)//static methodno parameters
+    }
+    public class DelegateExercises
+    {
+        public delegate int MyDelegate();
+        void Method1()
         {
-            Console.WriteLine("Method 2 executed");
-            Console.WriteLine("You entered "+x);
-            Console.ReadLine();
+            Console.WriteLine("my delegate");
         }
-
-        public static void Method3(int num2)
+        
+        public void Method3()
         {
-            Console.WriteLine(num2 * 10);
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
         }
     }
-    }
+}
 
