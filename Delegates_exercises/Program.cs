0135e65752bec982f74bd6df1b548b7d9038e40e
visualstudio <visualstudio@122-PC34>
2019-03-08 20:50:28 -0500

    lab12

--- Delegates_exercises/Program.cs
+++ Delegates_exercises/Program.cs
@@ -1,41 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-//Arun bhatia(C0732127)
-//Harjeet Singh(C0730468)
-namespace Delegates_exercises
+
+
+namespace DelegatesAndEvents
 {
-    class program
+    using System;
+
+    delegate void ExampleDelegate(string xyz);
+
+    class Program
     {
-        public static void Main(String[] args)
+        public static void Method1(string xyz)
         {
-            DelegateExercises delegatesExercises = new DelegateExercises();
-            delegatesExercises.Method3();
+            Console.WriteLine(xyz + " Method1");
+        }
+
+        public static void Method2(string xyz)
+        {
+            Console.WriteLine(xyz + " Method2");
+        }
+
+        public static void Main()
+        {
+            ExampleDelegate ex1Delegate, ex2Delegate, ex3Delegate, myDelegate;
+
+            ex1Delegate = new ExampleDelegate(Method1);
+            ex2Delegate = new ExampleDelegate(Method2);
+            ex3Delegate = ex1Delegate + ex2Delegate;
+            myDelegate = ex1Delegate - ex2Delegate;
+            ex1Delegate("AAA");
+            ex2Delegate("BBB");
+            ex3Delegate("CCC");
+            myDelegate("DDD");
+            myDelegate = ex3Delegate - ex1Delegate;
+            myDelegate("EEE");
+            myDelegate = ex3Delegate - ex2Delegate;
+            myDelegate("FFF");
             Console.ReadLine();
-
         }
     }
-    public delegate void Mydelegate();
-    public class DelegateExercises
-        {
-            
-            void Method1()
-            {
-            Console.WriteLine("method1");
-            }
-
-            public void Method3()
-            {
-                Mydelegate myDelegate = new Mydelegate(Method1);
-            myDelegate();
-            Console.WriteLine(myDelegate.ToString());
-            }
-        }
-    }
-
-    
+}
 
 
 
