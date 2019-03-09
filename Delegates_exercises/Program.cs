0a32942725f3f71f74f97f4cd5d5d3773303ca6e
visualstudio <visualstudio@122-PC34>
2019-03-08 20:41:52 -0500

    lab10

--- Delegates_exercises/Program.cs
+++ Delegates_exercises/Program.cs
@@ -9,13 +9,29 @@ namespace Delegates_exercises
 {
     class program
     {
-        
+        public static void Main(String[] args)
+        {
+            DelegateExercises delegatesExercises = new DelegateExercises();
+            delegatesExercises.Method3();
+            Console.ReadLine();
+
+        }
     }
     public delegate void Mydelegate();
-    public class DelegateExercises:Mydelegate
+    public class DelegateExercises
         {
             
-            
+            void Method1()
+            {
+            Console.WriteLine("method1");
+            }
+
+            public void Method3()
+            {
+                Mydelegate myDelegate = new Mydelegate(Method1);
+            myDelegate();
+            Console.WriteLine(myDelegate.ToString());
+            }
         }
     }
 
