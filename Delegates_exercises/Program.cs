2a51ff1a01f8327e785f7c8cacef3074bca0b501
visualstudio <visualstudio@122-PC34>
2019-03-08 21:05:32 -0500

    Lab14

--- Delegates_exercises/Program.cs
+++ Delegates_exercises/Program.cs
@@ -1,39 +1,41 @@
 ﻿using System;
-
+ 
 namespace DelegatesAndEvents
 {
-    public class Program
+  public class Program
+  {
+    public static void Main()
     {
-        public static void Main()
-        {
-            DelegateExercises delegateExercises = new DelegateExercises();
-            delegateExercises.Method3();
-            Console.ReadLine();
-        }
+      DelegateExercises delegateExercises = new DelegateExercises();
+      try
+      {
+        delegateExercises.Method3();
+        Console.ReadLine();
+      }
+      catch (System.Exception ex)
+      {
+        System.Console.WriteLine("Exception Occurred.");
+        Console.ReadLine();
+ 
+      }
     }
-
-    public delegate int MyDelegate(out int i);
-
-    public class DelegateExercises
+  }
+ 
+  public delegate void MyDelegate();
+ 
+  public class DelegateExercises
+  {
+    void Method1()
     {
-        int Method1(out int i)
-        {
-            System.Console.WriteLine("Method1");
-            i = 10;
-            return 0;
-        }
-
-        public void Method3()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            MyDelegate myDelegate1 = new MyDelegate(Method1);
-            MyDelegate myDelegate2 = myDelegate + myDelegate1;
-            int intValue;
-            myDelegate2(out intValue);
-
-        }
+      throw new System.Exception();
     }
-
+ 
+    public void Method3()
+    {
+      MyDelegate myDelegate = new MyDelegate(Method1);
+      myDelegate();
+    }
+  }
+ 
 }
-
 
